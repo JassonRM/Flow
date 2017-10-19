4a9b488841cadc16f1ff5efbee9014d47080d931
JassonRM <jassonrm@icloud.com>
2017-10-18 23:04:06 -0600

    Funciones para actualizar el dibujo de acuerdo con el metodo

--- src/org/tec/datos1/flow/parts/DiagramView.java
+++ src/org/tec/datos1/flow/parts/DiagramView.java
@@ -21,29 +21,28 @@ import org.tec.datos1.flow.graphics.While;
 
 
 public class DiagramView {
+	Canvas canvas;
+	//Variable con clase que se va a dibujar
+	
 	@Inject
 	public DiagramView(Composite parent) {
-		ScrolledComposite container = new ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL);
+		//Aqui debe llamar a la funcion de AST y pasarle la referencia a esta instancia
 		
-		Canvas canvas = new Canvas(container, SWT.NONE);
+		ScrolledComposite container = new ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL);
+		this.canvas = new Canvas(container, SWT.NONE);
 		canvas.setSize(1000, 1000);
 		container.setContent(canvas);
 		
 		canvas.addPaintListener(new PaintListener() {
 			@Override
 			public void paintControl(PaintEvent e) {
-				Rectangle clientArea = parent.getClientArea();
-				Method metodo = new Method(e.gc, "System.out.println(\"Hola mundo\")", clientArea.width / 2, 20);
-				Process process = new Process(e.gc, "x = 1", clientArea.width / 2, 100);
-				Line line1 = new Line(e.gc, metodo.getOutput(), process.getInput());
-				If condicional = new If(e.gc, "a <= 1", clientArea.width / 2, 200);
-				Line line2 = new Line(e.gc, process.getOutput(), condicional.getInput());
-				While ciclo = new While(e.gc, "cont <= 100", clientArea.width * 2 / 3, 400);
-				Line line3 = new Line(e.gc, condicional.getOutputFalse(), ciclo.getInput());
-				Line line4 = new Line(e.gc, ciclo.getOutputTrue(), ciclo.getInputReturn());
+				//Codigo donde que indica que se va a dibujar
 			}
-			
 		});
-		
+	}
+	
+	public void draw() { // Recibe la clase que se va a dibujar
+		//Guarda la clase que se va a dibujar
+		canvas.redraw();
 	}
 }
